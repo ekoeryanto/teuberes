d4641310c3446e742c09f302188567080bd21cdb
ekoeryanto <ekoeryanto@gmail.com>
2023-10-11 16:47:46 +0700

    chore: restyle

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,18 @@
 import './style.css'
 // import typescriptLogo from './typescript.svg'
 // import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+// import { setupCounter } from './counter.ts'
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
+  <div class="wall">
     
     <img class="lunas" src="/lunas.svg" class="logo vanilla" alt="Lunas" />
     <h1>teuberes.com</h1>
 
     <div class="read-the-docs">
-      <p>Beberapa hari lagi saya akan menuliskan pengalaman dalam merenovasi rumah di Griya Family IV</p>
+      <p>Beberapa hari lagi saya akan sharing pengalaman dalam merenovasi rumah di Griya Family IV</p>
     </div>
   </div>
 `
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+// setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
