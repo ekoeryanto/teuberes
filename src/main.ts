6f325ac08daa85edddef65758c649c64d9ab0b16
ekoeryanto <ekoeryanto@gmail.com>
2023-10-11 16:28:47 +0700

    chore: initialize

---
+++ src/main.ts
@@ -0,0 +1,18 @@
+import './style.css'
+// import typescriptLogo from './typescript.svg'
+// import viteLogo from '/vite.svg'
+import { setupCounter } from './counter.ts'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  <div>
+    
+    <img class="lunas" src="/lunas.svg" class="logo vanilla" alt="Lunas" />
+    <h1>teuberes.com</h1>
+
+    <div class="read-the-docs">
+      <p>Beberapa hari lagi saya akan menuliskan pengalaman dalam merenovasi rumah di Griya Family IV</p>
+    </div>
+  </div>
+`
+
+setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
